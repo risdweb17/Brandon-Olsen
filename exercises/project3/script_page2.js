1ce11368ff3cedfd6aa436e5ee05376c224c59c2
olsenbrandon <brandontolsen@Brandons-MBP.fios-router.home>
2017-01-24 01:27:15 -0500

    again

--- exercises/project3/script_page2.js
+++ exercises/project3/script_page2.js
@@ -3,15 +3,21 @@
 //});
 
 $(document).ready(function() {
-	$("body").removeClass(".backImage_1a").delay(300).queue(function(next){
+	$("body").removeClass(".backImage_1a").delay(400).queue(function(next){
     $(this).addClass("backImage_1b");
     next();
 	})
 });
 
 $(document).ready(function() {
-	$("body").removeClass(".backImage_1b").delay(300).queue(function(next){
+	$("body").removeClass(".backImage_1b").delay(400).queue(function(next){
     $(this).addClass("backImage_2");
     next();
 	})
 });
+
+//$(document).ready(function() {
+//	$(".final").click(function() {
+//		$(".final_hide2").fadeToggle("final_2click");
+//	})
+//});
--- exercises/project3/script_page3.js
+++ exercises/project3/script_page3.js
@@ -3,14 +3,14 @@
 //});
 
 $(document).ready(function() {
-	$("body").removeClass(".backImage_2a").delay(250).queue(function(next){
+	$("body").removeClass(".backImage_2a").delay(400).queue(function(next){
     $(this).addClass("backImage_2b");
     next();
 	})
 });
 
 $(document).ready(function() {
-	$("body").removeClass(".backImage_2b").delay(250).queue(function(next){
+	$("body").removeClass(".backImage_2b").delay(400).queue(function(next){
     $(this).addClass("backImage_3");
     next();
 	})
