fd821e482c98b962cd3310c672d8ff50bdfa9517
olsenbrandon <brandontolsen@brandons-mbp.risd.edu>
2017-01-23 20:17:30 -0500

    again

--- exercises/project3/script_page2.js
+++ exercises/project3/script_page2.js
@@ -3,14 +3,14 @@
 //});
 
 $(document).ready(function() {
-	$("body").removeClass(".backImage_1a").delay(250).queue(function(next){
+	$("body").removeClass(".backImage_1a").delay(300).queue(function(next){
     $(this).addClass("backImage_1b");
     next();
 	})
 });
 
 $(document).ready(function() {
-	$("body").removeClass(".backImage_1b").delay(250).queue(function(next){
+	$("body").removeClass(".backImage_1b").delay(300).queue(function(next){
     $(this).addClass("backImage_2");
     next();
 	})
