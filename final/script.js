dfaf050b43ed9a0f574eff6a9cadad4f2895ebfc
olsenbrandon <brandontolsen@Brandons-MBP.fios-router.home>
2017-01-30 20:42:21 -0500

    final exercise

--- final/script.js
+++ final/script.js
@@ -4,29 +4,3 @@
 //	});
 //});
 
-$(document).ready(function() {
-	$(".hamburger").click(function() {
-		$(".menuhide").slideToggle("menu");
-
-	})
-});
-
-$(document).ready(function() {
-$( ".open" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".open" ).show();
-});
-});
-});
-
-$(document).ready(function() {
-$( ".open" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".open" ).hide();
-$( ".hamburger" ).show();
-});
-});
-});
